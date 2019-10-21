bbab99b380a4d8c95f1994fed04fc27563025e66
Saloni <saloniskalsekar@gmail.com>
2019-10-20 23:28:57 -0500

    update

--- 937_reorder_data_in_logfiles.py
+++ 937_reorder_data_in_logfiles.py
@@ -1,3 +1,19 @@
+# class Solution:
+#     def reorderLogFiles(self, logs: List[str]) -> List[str]:
+#         digList = []
+#         alphList = []
+#         finalList = []
+#         for i in logs:
+#             log = i.split()
+#             if log[1].isdigit():
+#                 digList.append(i)
+#             else:
+#                 alphList.append(i)
+#
+#         alphList = sorted(alphList, key=lambda x: x.split()[1:] + [x.split()[0]])
+#         return (alphList + digList)
+
+
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
         digList = []
@@ -10,5 +26,11 @@ class Solution:
             else:
                 alphList.append(i)
 
-        alphList = sorted(alphList, key=lambda x: x.split()[1:] + [x.split()[0]])
+        alphList = sorted(alphList, key=lambda x: (x.split()[1:], x.split()[0]))
         return (alphList + digList)
+
+
+
+
+    # for k,v in sorted(d.items(), key = lambda x:(-x[0] ,  x[1]))
+    # minus only works on integer and reverses it
