28f3f04719bef6a5ba2779f1c745da67bd4a7474
Saloni <saloniskalsekar@gmail.com>
2019-11-06 20:50:16 -0600

    dfs

---
+++ 106_construct_binary_tree_from_inorder_postorder.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if inorder:
+            index = inorder.index(postorder.pop())
+            root = TreeNode(inorder[index])
+
+            root.right = self.buildTree(inorder[index + 1:], postorder)
+            root.left = self.buildTree(inorder[:index], postorder)
+
+            return root
---
+++ 116_populating_next_right_pointers_in_each_node.py
@@ -0,0 +1,45 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from collections import defaultdict
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+
+        if not root:
+            return
+
+        temp = defaultdict(list)
+
+        self.bfs(root, 1, temp)
+        for k, v in temp.items():
+            j = 0
+
+            while j < len(v):
+                if j == len(v) - 1:
+                    v[j].next = None
+                else:
+                    v[j].next = v[j + 1]
+
+                j += 1
+
+        return (root)
+
+    def bfs(self, node, level, temp):
+
+        if not node:
+            return
+
+        temp[level].append(node)
+
+        self.bfs(node.left, level + 1, temp)
+        self.bfs(node.right, level + 1, temp)
+
+        return
