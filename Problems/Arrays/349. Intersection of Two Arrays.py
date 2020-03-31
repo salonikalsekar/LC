f128d1892e5d53569b7970ef44fc3d2d3489c7bf
Saloni <saloniskalsekar@gmail.com>
2020-03-30 23:52:47 -0400

    arrays_fb

--- Problems/Arrays/349. Intersection of Two Arrays.py
+++ Problems/Arrays/349. Intersection of Two Arrays.py
@@ -3,3 +3,13 @@ class Solution:
         nums1Set = set(nums1)
         nums2Set = set(nums2)
         return nums1Set & nums2Set
+
+# Facebook interview question
+# O(n) time and O(1) space (the resulting array of intersections is not taken into consideration).
+# You are told the lists are sorted.
+# Cases to take into consideration include:
+# duplicates, negative values, single value lists, 0's, and empty list arguments.
+# Other considerations might include
+# sparse arrays.
+
+# ..to be continued
