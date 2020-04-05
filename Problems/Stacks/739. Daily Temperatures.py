198ceab26defe5ed5cd5900e20d4bb46b5fbf3c9
Saloni <saloniskalsekar@gmail.com>
2020-04-05 01:25:26 -0400

    stack

--- Problems/Stacks/739. Daily Temperatures.py
+++ Problems/Stacks/739. Daily Temperatures.py
@@ -1,19 +1,14 @@
 class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
         stack = []
-        res = {}
-        for i, t in enumerate(T):
+        res = [0 for i in range(len(T))]
+        for i,t in enumerate(T):
             while len(stack) > 0:
-                if stack[-1] < t:
-                    idx = T.index(stack[-1])
+                if stack[-1][0] < t:
+                    idx = stack[-1][1]
                     res[idx] = i - idx
                     stack.pop()
                 else:
                     break
-            stack.append(t)
-
-        if stack:
-            for i in stack:
-                res[T.index(stack[-1])] = 0
-        print(res)
-
+            stack.append((t,i))
+        return res
