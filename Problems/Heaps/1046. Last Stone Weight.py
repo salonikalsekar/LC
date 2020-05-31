5166eb74c29ba42243a9121e21316b4d684735bc
Saloni <saloniskalsekar@gmail.com>
2020-05-30 23:21:56 -0400

    heaps

---
+++ Problems/Heaps/1046. Last Stone Weight.py
@@ -0,0 +1,23 @@
+import heapq
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+
+        for i in range(len(stones)):
+            stones[i] = -stones[i]
+
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+
+            stone1 = heapq.heappop(stones)
+            stone2 = heapq.heappop(stones)
+
+            if stone1 != stone2:
+                heapq.heappush(stones, stone1 - stone2)
+
+        if stones:
+            return -heapq.heappop(stones)
+        else:
+            return 0
