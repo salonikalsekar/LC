4106bec9d6709ab3d5e2ed7f500a660b7cee11ad
Saloni <saloniskalsekar@gmail.com>
2020-11-29 12:43:14 -0500

    traversal

---
+++ 2020/145_PostOrderTraversalBST.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        stack = []
+        final = []
+        stack.append((root, False))
+
+        while stack:
+            curr, visited = stack.pop()
+            if curr:
+                if visited:
+                    final.append(curr.val)
+                else:
+                    stack.append((curr, True))
+                    stack.append((curr.right, False))
+                    stack.append((curr.left, False))
+
+        return final
