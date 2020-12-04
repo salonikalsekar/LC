47a9d5be0b0e1ebee3c3a0eead45d2485799c9ab
Saloni <saloniskalsekar@gmail.com>
2020-12-03 19:46:47 -0500

    warmup 2020

---
+++ 2020/242_ValidAnagram.py
@@ -0,0 +1,7 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) == len(t):
+            if sorted(s) == sorted(t):
+                return True
+        return False
