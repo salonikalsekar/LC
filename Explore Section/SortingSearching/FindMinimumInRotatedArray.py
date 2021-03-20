80afe8937f79026ca255f0ce28c262d1fb7bba9e
Saloni <saloniskalsekar@gmail.com>
2021-03-20 19:14:11 -0400

    random

---
+++ Explore Section/SortingSearching/FindMinimumInRotatedArray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        n = len(nums) - 1
+        r = n
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if mid < n and nums[mid - 1] > nums[mid]:
+                return nums[mid]
+
+            if nums[mid] > nums[r]:
+                l = mid + 1
+
+            else:
+                if nums[mid] < nums[r]:
+                    r = mid - 1
+                else:
+                    r = r - 1
+
+        return nums[l]
