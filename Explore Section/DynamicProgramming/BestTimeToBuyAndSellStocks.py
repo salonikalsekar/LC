069d09d63e7ecfa09e94542e5676ab9d4f5ad3c4
Saloni <saloniskalsekar@gmail.com>
2021-03-21 01:50:26 -0400

    random

---
+++ Explore Section/DynamicProgramming/BestTimeToBuyAndSellStocks.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_price = 0
+        min_price = float('inf')
+
+        for price in prices:
+            min_price = min(min_price, price)
+            max_price = max(max_price, price - min_price)
+
+        return max_price
