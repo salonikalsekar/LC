e1dbc0a739aa27969c681f4a071c1290d76734d7
Saloni <saloniskalsekar@gmail.com>
2021-03-21 22:04:42 -0400

    random

---
+++ Explore Section/Other/ExcelSheetColumnNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        res = 0
+
+        for i in range(len(columnTitle)):
+            res = res * 26
+            res += ord(columnTitle[i]) - ord('A') + 1
+
+        return res
