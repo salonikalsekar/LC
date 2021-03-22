8ea26f28a8f3ff04edd0a663bcba56cff8c24904
Saloni <saloniskalsekar@gmail.com>
2021-03-21 20:24:51 -0400

    random

---
+++ Explore Section/Other/SingleNumber.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        temp = Counter(nums)
+
+        for k, v in temp.items():
+            if v == 1:
+                return k
