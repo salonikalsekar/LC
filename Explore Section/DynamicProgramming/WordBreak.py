f56a9411b270076474b8154346457a713e3a9388
Saloni <saloniskalsekar@gmail.com>
2021-03-29 20:47:37 -0400

    random

---
+++ Explore Section/DynamicProgramming/WordBreak.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        visited = []
+        wordSet = set(wordDict)
+        q = deque()
+
+        q.append(0)
+
+        while q:
+            start = q.popleft()
+            if start in visited:
+                continue
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in wordSet:
+                    q.append(end)
+                    if end == len(s):
+                        return True
+                    visited.append(start)
+
+        return False
