df276f50ff8bc5b6af09bbf72cc16b3267ddc88f
Saloni <saloniskalsekar@gmail.com>
2021-08-28 15:11:14 -0400

    palindrome;;

---
+++ Top_Qs_100/palindrom_ll.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        res = []
+
+        while head:
+            res.append(head.val)
+            head = head.next
+
+        return res == res[::-1]
