3eed89abb53b359d8667e33317e253589b1b32e9
Saloni <saloniskalsekar@gmail.com>
2021-09-05 14:15:27 -0400

    cs

---
+++ Top_Qs_100/palindromic_substring.py
@@ -0,0 +1,19 @@
+_sclass Solution:
+    def countSubstrings(self, s: str) -> int:
+        c = 0
+        def expandAroundCenter(i, j, s):
+            a = 0
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+                a += 1
+
+
+            return a
+
+        for i in range(len(s)):
+            c += expandAroundCenter(i, i, s) # 1 3
+            c += expandAroundCenter(i, i+1, s) # 2 2
+
+        return c
+
