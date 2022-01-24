8b5175e06f4c66dd29ab5f9dda1925017ee8559f
Saloni Kalsekar <saloniskalsekar@gmail.com>
2022-01-24 08:46:03 -0500

    nlogk heaps

--- 2019/347_top_k_frequent_elements.py
+++ 2019/347_top_k_frequent_elements.py
@@ -42,7 +42,7 @@ class Solution:
 
 ---------------------------------------
 Solution 3
-Using heaps 
+Using heaps - nlogn
 
 from collections import Counter
 import heapq
@@ -56,3 +56,28 @@ class Solution:
             heapq.heappush(q, (-1 * value, key))
                  
         return [heappop(q)[1] for _ in range(k)]
+
+-------------------------------------------------------------------------------------------------------------------------
+Solution 4 - heaps O(nlogk)
+
+import heapq as h
+class Solution:
+    def topKFrequent(self, arr: List[int], k: int) -> List[int]:
+        # Make counter dictionary
+        count_dict = Counter(arr)
+        items = []
+        # Revert the key, value order since tuple is sorted by first value in heap
+        for key,val in count_dict.items():
+            items.append((val,key))
+        heap = []
+        # push first k elements in heap
+        for i in range(k):
+            h.heappush(heap,items[i])
+        # Push rest of elements by maintaing length = k
+        for i in range(k, len(items)):
+            
+            if items[i][0] >= heap[0][0]:
+                h.heappop(heap)
+                h.heappush(heap, items[i])
+        # Return the values of the tuples
+        return [x[1] for x in heap]
